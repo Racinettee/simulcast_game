300107c3b3fa4276120ca0b4908eeee5e2369e72
Racinettee <StevePhillipsCa@gmail.com>
2022-02-22 09:48:32 -0800

    add color profile, external file chunks

--- pkg/simul_ase/ase_binary/simul_ase.go
+++ pkg/simul_ase/ase_binary/simul_ase.go
@@ -29,7 +29,8 @@ type AsepriteString struct {
  *   RGBA: BYTE[4], each pixel have 4 bytes in this order Red, Green, Blue, Alpha.
  *   Grayscale: BYTE[2], each pixel have 2 bytes in the order Value, Alpha.
  *   Indexed: BYTE, each pixel uses 1 byte (the index).
- * TILE: Tilemaps: Each tile can be a 8-bit (BYTE), 16-bit (WORD), or 32-bit (DWORD) value and there are masks related to the meaning of each bit.
+ * TILE: Tilemaps: Each tile can be a 8-bit (BYTE), 16-bit (WORD), or 32-bit (DWORD) value and there are masks
+ * related to the meaning of each bit.
  *
  * File Header:
  * DWORD     File size
@@ -232,3 +233,229 @@ type AsepriteLayerChunk2004 struct {
 	// + if layer type = 2
 	TilesetIndex uint32
 }
+
+/**
+ * Cel Chunk (0x2005)
+ * This chunk determine where to put a cel in the specified layer/frame.
+ *
+ * WORD        Layer index (see NOTE.2)
+ * SHORT       X position
+ * SHORT       Y position
+ * BYTE        Opacity level
+ * WORD        Cel Type
+ *           0 - Raw Image Data (unused, compressed image is preferred)
+ *           1 - Linked Cel
+ *           2 - Compressed Image
+ *           3 - Compressed Tilemap
+ * BYTE[7]     For future (set to zero)
+ * + For cel type = 0 (Raw Image Data)
+ *  WORD      Width in pixels
+ *  WORD      Height in pixels
+ *  PIXEL[]   Raw pixel data: row by row from top to bottom,
+ *           for each scanline read pixels from left to right.
+ * + For cel type = 1 (Linked Cel)
+ *  WORD      Frame position to link with
+ * + For cel type = 2 (Compressed Image)
+ *  WORD      Width in pixels
+ *  WORD      Height in pixels
+ *  BYTE[]    "Raw Cel" data compressed with ZLIB method (see NOTE.3)
+ * + For cel type = 3 (Compressed Tilemap)
+ *  WORD      Width in number of tiles
+ *  WORD      Height in number of tiles
+ *  WORD      Bits per tile (at the moment it's always 32-bit per tile)
+ *  DWORD     Bitmask for tile ID (e.g. 0x1fffffff for 32-bit tiles)
+ *  DWORD     Bitmask for X flip
+ *  DWORD     Bitmask for Y flip
+ *  DWORD     Bitmask for 90CW rotation
+ *  BYTE[10]  Reserved
+ *  TILE[]    Row by row, from top to bottom tile by tile
+ *            compressed with ZLIB method (see NOTE.3)
+ */
+
+type AsepriteCelChunk2005 struct {
+	LayerIndex   uint16
+	X, Y         int16
+	OpacityLevel byte
+	CelType      uint16
+	future       [7]byte
+	// + For cel type = 0 (Raw Image Data)
+	WidthInPix, HeightInPix uint16
+	RawPixData              []byte
+	// + For cel type = 1 (Linked Cel)
+	FramePosToLinkWith uint16
+	// + For cel type = 2 (Compressed Image)
+	// WORD      Width in pixels
+	// WORD      Height in pixels
+	RawCelData []byte // "Raw Cel" data compressed with ZLIB method (see NOTE.3)
+	// + For cel type = 3 (Compressed Tilemap)
+	WidthInTiles, HeightInTiles uint16
+	BitsPerTile                 uint16 // (at the moment it's always 32-bit per tile)
+	BitMaskForTileID            uint32 // (e.g. 0x1fffffff for 32-bit tiles)
+	BitMaskForXFlip             uint32
+	BitMaskForYFlip             uint32
+	BitMaskFor90CWRot           uint32
+	reserved                    [10]byte
+	Tiles                       []byte // zlib data (see NOTE.3)
+}
+
+/**
+ * Cel Extra Chunk (0x2006)
+ * Adds extra information to the latest read cel.
+ *
+ * DWORD     Flags (set to zero)
+ *            1 = Precise bounds are set
+ * FIXED     Precise X position
+ * FIXED     Precise Y position
+ * FIXED     Width of the cel in the sprite (scaled in real-time)
+ * FIXED     Height of the cel in the sprite
+ * BYTE[16]  For future use (set to zero)
+ */
+type AsepriteCelExtraChunk2006 struct {
+	Flags              uint32
+	PreciseX, PreciseY uint32
+	WidthCelInSprite   uint32
+	HeightCelInSprite  uint32
+	futureUse          [16]byte
+}
+
+/**
+ * Color Profile Chunk (0x2007)
+ * Color profile for RGB or grayscale values.
+ *
+ * WORD        Type
+ *              0 - no color profile (as in old .aseprite files)
+ *              1 - use sRGB
+ *              2 - use the embedded ICC profile
+ * WORD        Flags
+ *              1 - use special fixed gamma
+ * FIXED       Fixed gamma (1.0 = linear)
+ *             Note: The gamma in sRGB is 2.2 in overall but it doesn't use
+ *             this fixed gamma, because sRGB uses different gamma sections
+ *             (linear and non-linear). If sRGB is specified with a fixed
+ *             gamma = 1.0, it means that this is Linear sRGB.
+ * BYTE[8]     Reserved (set to zero)
+ * + If type = ICC:
+ * DWORD     ICC profile data length
+ * BYTE[]    ICC profile data. More info: http://www.color.org/ICC1V42.pdf
+ */
+
+type AsepriteColorProfileChunk2007 struct {
+	Type       uint16
+	Flags      uint16
+	FixedGamma uint32
+	reserved   [8]byte
+	// + If type = ICC:
+	ICCProfileDatLen uint32
+	ICCProfileDat    []byte
+}
+
+/**
+ * External Files Chunk (0x2008)
+ * A list of external files linked with this file. It might be used to reference external palettes or tilesets.
+ *
+ * DWORD       Number of entries
+ * BYTE[8]     Reserved (set to zero)
+ * + For each entry
+ *  DWORD     Entry ID (this ID is referenced by tilesets or palettes)
+ *  BYTE[8]   Reserved (set to zero)
+ *  STRING    External file name
+ */
+
+type AsepriteExternalFilesChunk2008 struct {
+	NumEntries uint32
+	reserved   [8]byte
+	// + for each entry
+	ExternalFile []AsepriteExternalFilesChunk2008Entry
+}
+
+type AsepriteExternalFilesChunk2008Entry struct {
+	EntryID          uint32
+	reserved         [8]byte
+	ExternalFilename AsepriteString
+}
+
+/**
+ * Mask Chunk (0x2016) DEPRECATED
+ *
+ * SHORT   X position
+ * SHORT   Y position
+ * WORD    Width
+ * WORD    Height
+ * BYTE[8] For future (set to zero)
+ * STRING  Mask name
+ * BYTE[]  Bit map data (size = height*((width+7)/8))
+ *         Each byte contains 8 pixels (the leftmost pixels are
+ *         packed into the high order bits)
+ */
+
+type AsepriteMaskChunk2016 struct {
+	X, Y          int16
+	Width, Height uint16
+	future        [8]byte
+	MaskName      AsepriteString
+	BitMapData    []byte
+}
+
+/**
+ * Path Chunk (0x2017)
+ * Never used
+ */
+
+type AsepritePathChunk2017 struct{}
+
+/**
+ * Tags Chunk (0x2018)
+ * After the tags chunk, you can write one user data chunk for each tag. E.g. if there are 10 tags, you can then write 10 user data chunks one for each tag.
+ *
+ * WORD        Number of tags
+ * BYTE[8]     For future (set to zero)
+ * + For each tag
+ *  WORD      From frame
+ *  WORD      To frame
+ *  BYTE      Loop animation direction
+ *              0 = Forward
+ *              1 = Reverse
+ *              2 = Ping-pong
+ *  BYTE[8]   For future (set to zero)
+ *  BYTE[3]   RGB values of the tag color
+ *              Deprecated, used only for backward compatibility with Aseprite v1.2.x
+ *              The color of the tag is the one in the user data field following
+ *              the tags chunk
+ *  BYTE      Extra byte (zero)
+ *  STRING    Tag name
+ */
+type AsepriteTagsChunk2018 struct {
+}
+
+/**
+ * Notes
+ * NOTE.1
+ * The child level is used to show the relationship of this layer with the last one read, for example:
+ *
+ * Layer name and hierarchy      Child Level
+ * -----------------------------------------------
+ * - Background                  0
+ *   `- Layer1                   1
+ * - Foreground                  0
+ *   |- My set1                  1
+ *   |  `- Layer2                2
+ *   `- Layer3                   1
+ *
+ * NOTE.2
+ * The layer index is a number to identify any layer in the sprite, for example:
+ *
+ * Layer name and hierarchy      Layer index
+ * -----------------------------------------------
+ * - Background                  0
+ *   `- Layer1                   1
+ * - Foreground                  2
+ *   |- My set1                  3
+ *   |  `- Layer2                4
+ *   `- Layer3                   5
+ *
+ * NOTE.3
+ * Details about the ZLIB and DEFLATE compression methods:
+ * https://www.ietf.org/rfc/rfc1950
+ * https://www.ietf.org/rfc/rfc1951
+ * Some extra notes that might help you to decode the data: http://george.chiramattel.com/blog/2007/09/deflatestream-block-length-does-not-match.html
+ */
