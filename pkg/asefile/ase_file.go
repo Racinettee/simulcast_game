1d822b5f930c5ec816d4b9552785ecc39c959382
Racinettee <StevePhillipsCa@gmail.com>
2022-02-25 00:35:51 +0000

    Initial decoding of header and frames

--- pkg/asefile/ase_file.go
+++ pkg/asefile/ase_file.go
@@ -10,4 +10,19 @@ type AsepriteFile struct {
 func (aseFile *AsepriteFile) Decode(r io.Reader) {
 	aseFile.Header.Decode(r)
 
+	aseFile.Frames = make([]AsepriteFrame, aseFile.Header.Frames)
+
+	for _, frame := range aseFile.Frames {
+		frame.Decode(r)
+	}
+}
+
+func (aseFile *AsepriteFile) Encode(w io.Writer) {
+	aseFile.Header.Encode(w)
+
+	aseFile.Frames = make([]AsepriteFrame, aseFile.Header.Frames)
+
+	for _, frame := range aseFile.Frames {
+		frame.Encode(w)
+	}
 }
