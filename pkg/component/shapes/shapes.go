45c8b5106e292b8c3bf327e69b900cf7b2b695c6
Racinettee <StevePhillipsCa@gmail.com>
2022-04-24 16:28:49 -0700

    Actually use collisgen ...

--- pkg/component/shapes/shapes.go
+++ pkg/component/shapes/shapes.go
@@ -1,4 +1,4 @@
 package shapes
 
 // The generate command runs from the directory this file is within, the sprite we want to examine is 3 layers backwards
-//go:generate collision_builder -i  -o chica-shapes.go -p shapes
+//go:generate collisgen -c
