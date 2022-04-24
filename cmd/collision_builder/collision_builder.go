f5538157c8ea1734683a5f0bd1df5389b0812a7d
Racinettee <StevePhillipsCa@gmail.com>
2022-04-23 17:06:26 -0700

    Refactor to support multiple layers

--- cmd/collision_builder/collision_builder.go
+++ cmd/collision_builder/collision_builder.go
@@ -9,10 +9,13 @@ import (
 	"unicode"
 
 	"github.com/Racinettee/asefile"
+	"github.com/Racinettee/generics"
 	"github.com/jessevdk/go-flags"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
 )
 
-type outFileTemplate struct {
+type outputShape struct {
 	FileName string
 	// For each frame (key) list of points (value)
 	Points map[int][]int
@@ -22,13 +25,13 @@ type outputTemplate struct {
 	// The package to insert into
 	Package string
 	// The list of files with respective frames -> shapes
-	Files []outFileTemplate
+	Files []outputShape
 }
 
 var outputTemplateStr string = `package {{.Package}}
 // This code is generated, do not edit
 {{ range .Files }}
-var {{.FileName}}Shapes = map[int][]int{ {{ range $frame, $points := .Points }}
+var {{.FileName}}Shape = map[int][]int{ {{ range $frame, $points := .Points }}
 	{{ $frame }}: { {{ range $index, $elem := $points }}{{ if $index }},{{ end }} {{ $elem }}{{ end }} },{{ end }}
 }
 {{ end }}`
@@ -54,82 +57,14 @@ func main() {
 	}
 	mainResult := outputTemplate{
 		Package: opts.Package,
-		Files:   make([]outFileTemplate, 0),
+		Files:   make([]outputShape, 0),
 	}
 
 	inFiles := strings.Split(opts.InFiles, ",")
 
 	// For each file in inFiles we parse the ase file looking for a collision layer
 	for _, file := range inFiles {
-		outResult := outFileTemplate{
-			FileName: transformTitle(file),
-			Points:   make(map[int][]int),
-		}
-		var aseFile asefile.AsepriteFile
-		err = aseFile.DecodeFile(file)
-		if err != nil {
-			panic(err)
-		}
-		if len(aseFile.Frames) == 0 {
-			println("No frames in file")
-			continue
-		}
-		collisLayer := -1
-		for x, layer := range aseFile.Frames[0].Layers {
-			if layer.LayerName == "<Collision>" {
-				collisLayer = x
-			}
-		}
-		if collisLayer == -1 {
-			println("No collision layers - use <Collision> to define collision layers")
-			continue
-		}
-		// For each frame we want to scan the pixels and find all the ones of a certain color
-		for framei, frame := range aseFile.Frames {
-			cel := frame.Cels[collisLayer]
-			dat := cel.RawCelData
-			if len(dat) == 0 {
-				continue
-			}
-			points := make([]Pointi, 0)
-			// For now, We'll just go with 255,0,255,255 as our color
-			w, h := cel.WidthInPix, cel.HeightInPix
-			offset := 0
-			for y := 0; y < int(h); y += 1 {
-				for x := 0; x < int(w); x, offset = x+1, offset+4 {
-					r, g, b, a := dat[offset], dat[offset+1], dat[offset+2], dat[offset+3]
-					if r == 255 && g == 0 && b == 255 && a == 255 {
-						points = append(points, Pointi{x, y})
-					}
-				}
-			}
-			// Sort the points first by X order
-			sort.Sort(ByPointX(points))
-			// Sort the highest and lowest points by the middle of the median y value
-			highs := make([]Pointi, 0)
-			lows := make([]Pointi, 0)
-
-			middle := int(h / 2)
-			for _, point := range points {
-				if point.Y <= middle {
-					highs = append(highs, point)
-				} else {
-					lows = append(lows, point)
-				}
-			}
-			result := make([]int, 0)
-			// With all high values (points near the top have lower y value)
-			// iterate them forward and generate the coordinates
-			for _, point := range highs {
-				// Add the point - but including the offset of the cel for the final data
-				result = append(result, point.X+int(cel.X), point.Y+int(cel.Y))
-			}
-			for i := len(lows) - 1; i >= 0; i-- {
-				result = append(result, lows[i].X+int(cel.X), lows[i].Y+int(cel.Y))
-			}
-			outResult.Points[framei] = result
-		}
-		mainResult.Files = append(mainResult.Files, outResult)
+		mainResult.Files = append(mainResult.Files, collectCollisionData(file)...)
 	}
 
 	t, err := template.New("shapes").Parse(outputTemplateStr)
@@ -146,7 +81,8 @@ func main() {
 }
 
 func transformTitle(title string) string {
-	str := strings.Title(strings.TrimSuffix(filepath.Base(title), filepath.Ext(title)))
+	caser := cases.Title(language.English)
+	str := caser.String(strings.TrimSuffix(filepath.Base(title), filepath.Ext(title)))
 	return strings.Map(func(r rune) rune {
 		switch {
 		case unicode.IsDigit(r), unicode.IsLetter(r):
@@ -154,4 +90,90 @@ func transformTitle(title string) string {
 		}
 		return -1
 	}, str)
+}
+
+func collectCollisionData(file string) []outputShape {
+	results := generics.NewList[outputShape](0)
+	// Load the ase file and quickly parse it for any layers that match our naming scheme
+	var aseFile asefile.AsepriteFile
+	err := aseFile.DecodeFile(file)
+	if err != nil {
+		panic(err)
+	}
+	if len(aseFile.Frames) == 0 {
+		println("No frames in file")
+		return results
+	}
+	// Figure out which layers we care about before parsing the frames
+	collisLayers := generics.NewList[generics.Pair[int, string]](0)
+	for x, layer := range aseFile.Frames[0].Layers {
+		if layer.LayerName == "<HurtBox>" {
+			collisLayers.Push(generics.NewPair(x, "_HurtBox"))
+		} else if layer.LayerName == "<HitBox>" {
+			collisLayers.Push(generics.NewPair(x, "_HitBox"))
+		}
+	}
+	// If there was no matching layers then return from this function to move on
+	if collisLayers.Len() == 0 {
+		println("No collision layers - use <HitBox> or <HurtBox> to define collision layers")
+		return results
+	}
+	// For each collision layer we need to scan the frames and collect all the points
+	for _, layer := range collisLayers {
+		results.Push(parseFrame(aseFile, file, layer))
+	}
+	return results
+}
+
+func parseFrame(aseFile asefile.AsepriteFile, file string, layer generics.Pair[int, string]) outputShape {
+	outResult := outputShape{
+		FileName: transformTitle(file) + layer.Right,
+		Points:   make(map[int][]int),
+	}
+	// For each frame we want to scan the pixels and find all the ones of a certain color
+	for framei, frame := range aseFile.Frames {
+		cel := frame.Cels[layer.Left]
+		dat := cel.RawCelData
+		if len(dat) == 0 {
+			continue
+		}
+		points := make([]Pointi, 0)
+		// For now, We'll just go with 255,0,255,255 as our color
+		w, h := cel.WidthInPix, cel.HeightInPix
+		offset := 0
+		for y := 0; y < int(h); y += 1 {
+			for x := 0; x < int(w); x, offset = x+1, offset+4 {
+				r, g, b, a := dat[offset], dat[offset+1], dat[offset+2], dat[offset+3]
+				if r == 255 && g == 0 && b == 255 && a == 255 {
+					points = append(points, Pointi{x, y})
+				}
+			}
+		}
+		// Sort the points first by X order
+		sort.Sort(ByPointX(points))
+		// Sort the highest and lowest points by the middle of the median y value
+		highs := make([]Pointi, 0)
+		lows := make([]Pointi, 0)
+
+		middle := int(h / 2)
+		for _, point := range points {
+			if point.Y <= middle {
+				highs = append(highs, point)
+			} else {
+				lows = append(lows, point)
+			}
+		}
+		result := make([]int, 0)
+		// With all high values (points near the top have lower y value)
+		// iterate them forward and generate the coordinates
+		for _, point := range highs {
+			// Add the point - but including the offset of the cel for the final data
+			result = append(result, point.X+int(cel.X), point.Y+int(cel.Y))
+		}
+		for i := len(lows) - 1; i >= 0; i-- {
+			result = append(result, lows[i].X+int(cel.X), lows[i].Y+int(cel.Y))
+		}
+		outResult.Points[framei] = result
+	}
+	return outResult
 }
--- cmd/collision_builder/go.mod
+++ cmd/collision_builder/go.mod
@@ -1,10 +1,12 @@
 module github.com/Racinettee/simulcast_game/cmd/collision_builder
 
-go 1.17
+go 1.18
 
 require (
 	github.com/Racinettee/asefile v0.0.0-20220227195809-005d6ba09e87
+	github.com/Racinettee/generics v0.0.0-20220423223536-9e335d463430
 	github.com/jessevdk/go-flags v1.5.0
+	golang.org/x/text v0.3.7
 )
 
-require golang.org/x/sys v0.0.0-20210917161153-d61c044b1678 // indirect
+require golang.org/x/sys v0.0.0-20220422013727-9388b58f7150 // indirect
