83da91c06681d39683dc01ccb6576aa378c2017b
Racinettee <StevePhillipsCa@gmail.com>
2022-04-23 17:14:03 -0700

    Use the newer collision builder

---
+++ pkg/component/shapes/chica-shapes.go
@@ -0,0 +1,12 @@
+package shapes
+// This code is generated, do not edit
+
+var ChicaSpeardown_HurtBoxShape = map[int][]int{ 
+	0: {  12, 21, 15, 21, 15, 28, 12, 28 },
+	1: {  10, 27, 15, 27, 15, 37, 10, 37 },
+}
+
+var ChicaSpeardown_HitBoxShape = map[int][]int{ 
+	0: {  0, 0, 15, 0, 15, 22, 0, 22 },
+	1: {  0, 0, 15, 0, 15, 22, 0, 22 },
+}
--- pkg/component/shapes/shapes.go
+++ pkg/component/shapes/shapes.go
@@ -1,4 +1,4 @@
 package shapes
 
 // The generate command runs from the directory this file is within, the sprite we want to examine is 3 layers backwards
-//go:generate cmd/collision_builder -i ../../sprites/player/Chica-SpearDown.aseprite -o chica-shapes.go -p shapes
+//go:generate collision_builder -i ../../../sprites/player/Chica-SpearDown.aseprite -o chica-shapes.go -p shapes
